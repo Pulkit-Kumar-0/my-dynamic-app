99126ce00f9267e7dbe1b6e1838396e87c6b9551
Pulkit Kumar <pulkitkumar2711@gmail.com>
2025-04-03 16:42:00 +0530

    coverted into static application

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white py-4 text-center mt-10">
+      &copy; 2025 My Static App. All rights reserved.
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="bg-blue-600 text-white py-4 text-center text-xl font-bold">
+      My Static Next.js App
+    </header>
+  );
+};
+
+export default Header;
