ea4e70ceaa2d66c46d90b114c157e792441ed246
Pulkit Kumar <pulkitkumar2711@gmail.com>
2025-04-03 18:28:09 +0530

    all

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-// next.config.mjs
-export default {
-  reactStrictMode: true,
-  output: "export", // This will enable static export
-  images: {
-    unoptimized: true, // Disable image optimization (important for static export)
-  },
-};
